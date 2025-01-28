3aad08e41518202fc3d0afafed0978c92118f92f
Kovaleva Marina <kirienko1995@mail.ru>
2025-01-28 15:57:10 +0300

    Add front for admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
 
-
-
+Route::prefix('admin')->group(function () {
+    Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
+});
 
 Auth::routes();
 
