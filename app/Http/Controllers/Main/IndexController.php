3dad2921bd2d55f54f4a6db8e9cbdf964e1f2e65
Kovaleva Marina <kirienko1995@mail.ru>
2025-02-13 16:10:22 +0300

    Add displaying a feed of posts. Random posts. Popular posts. Pagination

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
     {
-        return view('main.index');
+        $posts = Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+
+        return view('main.index', compact('posts', 'randomPosts', 'likedPosts'));
     }
 }
